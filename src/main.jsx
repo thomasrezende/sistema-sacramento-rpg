19cc5258777f99414d5fefaecf3a3befdde3debd
Thomas Rezende <threzendesantos@gmail.com>
2026-03-14 21:13:20 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './prata-e-chumbo-rpg.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
